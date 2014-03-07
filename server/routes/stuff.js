7d1b8510bd2369ccb211bbd2597c6d0ffdef1948
Timothy McKenna <mckenna.tim@gmail.com>
2014-03-07 13:15:58 -0500

    no routes just skeleton

--- server/routes/stuff.js
+++ server/routes/stuff.js
@@ -25,120 +25,7 @@ mongoClient.open(function(err, mongoClient) {
     });    
 });
 
-exports.findById = function(req, res) {
-    console.log('in find by id');
-    console.log(req.params);
-    var id = req.params.id;
-    var list = req.params.list;
-    console.log(typeof id);
-    var ObjectId = require('mongoose').Types.ObjectId;
-    db.collection(list, function(err, collection) {
-        collection.find({_id:ObjectId(id)}).toArray(function(err, items) {
-            //console.log(items);
-            res.jsonp(items);
-        });
-    });
-};
 
-exports.findByName = function(req, res) {
-    console.log('in find by name');
-    console.log(req.params);
-    var name = req.params.name;
-    db.collection('users', function(err, collection) {
-        collection.find({name:name}).toArray(function(err, items) {
-            //console.log(items);
-            res.jsonp(items);
-        });
-    });
-};
-
-var find =function(na, res){
-    db.collection(na, function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            console.log(items);
-            res.jsonp(items);
-        });
-    });
-};
-
-exports.findLists = function(req, res) {
-    console.log('in findLists');
-    find('lists', res);
-};
-
-exports.findUsers = function(req, res) {
-    console.log('in findUsers');
-    find('users', res);
-};
-
-exports.findItems = function(req, res) {
-    console.log('in findItem');
-    find('items', res);
-};
-
-
-exports.createUser = function(req, res){
-    //res.cookie('name', 'tobi', { domain: 'localhost', path: '/framewks/rest', secure: true });
-    var body= req.body; 
-    db.collection('users', function(err, collection) {
-        collection.insert(body, function(err, saved) {
-            console.log(saved);
-            res.jsonp(saved);
-        });
-    });      
-};
-
-exports.getUsersLists = function (req, res) {//lists/:name
-    console.log('in getUsersLists');
-    var name =req.params.name;
-    console.log(name);
-    db.collection('users', function(err, collection) {
-        collection.find({name:name},{lists:1}).toArray(function(err, items) {
-            //console.log(items);
-            res.jsonp(items);
-        });
-    });   
-};
-
-exports.addList2user = function(req, res){
-    console.log('in addList2user');
-    var name =req.params.name; 
-    var lid =req.params.lid;
-    var ObjectId = require('mongoose').Types.ObjectId;
-    db.collection('lists', function(err, collection) {
-        collection.findOne({_id:ObjectId(lid)}, function(err, items) {
-            userlist=(items);
-            db.collection('users', function(err, collection) {
-                collection.update({name:name},{$push:{lists:userlist}}, {upsert:false}, function(err, saved) {
-                    res.jsonp(saved);
-                });
-            });
-            res.jsonp('saved');
-        });
-    });
-};
-
-exports.getListItems4user = function(req, res){
-    console.log('in getListItems4user');
-    var user =req.params.user; 
-    var list =req.params.list;
-    db.collection('users', function(err, collection) {
-        collection.findOne({name:user}, function(err, auser) {
-            var userlists=auser.lists;
-            obj = _un.find(userlists, function(obj) { return obj.name == 'groceries'});
-            console.log(obj._id);
-            /*
-            db.collection('items', function(err, collection) {
-                collection.find({name:name},{lists:1}).toArray(function(err, items) {
-            
-                    res.jsonp(saved);
-                });
-            });
-*/
-            res.jsonp(obj._id);
-        });
-    });
-};
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
@@ -157,29 +44,30 @@ var populateDB = function(huh) {
 var items =[];
 items.name = 'items';
 items.items = [   
-{lid:'5315545dafcb3c583086f86d',item:'banana', done:false},
-{lid:'lid5315545dafcb3c583086f86d',item:'coffee', done:false},
-{lid:'lid5315545dafcb3c583086f86d',item:'brown sugar', done:false},
-{lid:'lid5315545dafcb3c583086f86d',item:'bacon', done:false},
-{lid:'lid5315545dafcb3c583086f86d',item:'apples', done:false},
-{lid:'lid5315545dafcb3c583086f86d',item:'brown gravy', done:true},
-{lid:'lid5315545dafcb3c583086f86d',item:'bags', done:true},
-{lid:'lid5315545dafcb3c583086f86d',item:'applesauce', done:true},
-{lid:'lid5315545dafcb3c583086f86d',item:'sugar', done:true},
-{lid:'lid5315545dafcb3c583086f86d',item:'baby back ribs', done:true},
-{lid:'lid5315545dafcb3c583086f86d',item:'apple butter', done:true}
+{lid:'1',item:'banana', done:false},
+{lid:'4',item:'coffee', done:false},
+{lid:'4',item:'brown sugar', done:false},
+{lid:'4',item:'bacon', done:false},
+{lid:'1',item:'apples', done:false},
+{lid:'5',item:'2x4-8\'', done:false},
+{lid:'1',item:'brown gravy', done:true},
+{lid:'7',item:'bags', done:true},
+{lid:'7',item:'applesauce', done:true},
+{lid:'4',item:'sugar', done:true},
+{lid:'1',item:'baby back ribs', done:true},
+{lid:'4',item:'apple butter', done:true}
 ];
 
 var lists =[];
 lists.name = 'lists';
 lists.items = [
-{name:'groceries'},
-{name:'hardware'},
-{name:'drugs'},
-{name:'groceries'},
-{name:'building'},
-{name:'garden'},
-{name:'groceries'}
+{lid:1, name:'groceries'},
+{lid:2, name:'hardware'},
+{lid:3, name:'drugs'},
+{lid:4, name:'groceries'},
+{lid:5, name:'building'},
+{lid:6, name:'garden'},
+{lid:7, name:'groceries'}
 ];
 
 var users = [];
--- server/server.js
+++ server/server.js
@@ -10,19 +10,5 @@ var app = express();
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/lists/:name', stuff.getUsersLists);
-app.post('/users', stuff.createUser);
-//app.post('/lists/:user/:lname', stuff.createList4user);
-app.put('/users/:name/:lid', stuff.addList2user);
-//app.put('/items/:lid', stuff.add2List);
-app.get('/lists', stuff.findLists);
-app.get('/users', stuff.findUsers);
-app.get('/items', stuff.findItems);
-app.get('/users/name/:name', stuff.findByName);
-app.get('/:list/id/:id', stuff.findById);
-app.get('/items/:user/:list', stuff.getListItems4user);
-//app.get('/items/:lid, stuff', stuff.)
-
-
 app.listen(3020);
 console.log('Listening on port 3020...');
