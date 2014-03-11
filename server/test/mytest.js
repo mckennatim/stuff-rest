446d2596066abd1643f3011af53525e3d81b9180
Timothy McKenna <mckenna.tim@gmail.com>
2014-03-11 00:10:39 -0400

    repopulates stuffTest

--- server/test/mytest.js
+++ server/test/mytest.js
@@ -1,4 +1,12 @@
-var app = require('../app.js')
-var config = require('../config/changeDb')
-config.change2db('../config/', 'test')
-app.listen(3020)
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/stuffTest', function(err, res){
+  mongoose.connection.db.dropDatabase(function(err, res){
+    mongoose.connection.close(function(err, res){
+      var app = require('../app.js');
+      var config = require('../config/change2db');
+      config.change2db('../config/', 'stuffTest');
+      app.listen(3020);      
+    });  
+  });
+})
+
