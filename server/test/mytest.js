96c228127b1d403fb53b39aab4ff3717bd2bda85
Timothy McKenna <mckenna.tim@gmail.com>
2014-03-10 21:49:50 -0400

    mess a disaster

--- server/test/mytest.js
+++ server/test/mytest.js
@@ -1,4 +1,4 @@
 var app = require('../app.js')
-var config = require('../config/changeDb')
-config.change2db('../config/', 'test')
+//var config = require('../config/change2db')
+//config.change2db('../config/', 'stuffTest')
 app.listen(3020)
